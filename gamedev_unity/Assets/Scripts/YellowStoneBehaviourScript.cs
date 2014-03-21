455241100f9ac8536af4c5f8c9a48b6379ac0b68
petrstudeny <hobb@seznam.cz>
2014-03-21 23:03:34 +0100

    Brutus hack

---
+++ gamedev_unity/Assets/Scripts/YellowStoneBehaviourScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YellowStoneBehaviourScript : MonoBehaviour {
+
+	private int stoneId = 0;
+	private int mySlot = 0;
+
+	// Use this for initialization
+	void Start () {
+		if (this.name == "yellowstone1") {
+			stoneId = 1; mySlot = 0;
+		}
+		else if (this.name == "yellowstone2") {
+			stoneId = 2; mySlot = 1;
+		}
+		else if (this.name == "yellowstone3") {
+			stoneId = 3; mySlot = 2;
+		}
+		else if (this.name == "greenstone1") {
+			stoneId = 6; mySlot = 6;
+		}
+		else if (this.name == "greenstone2") {
+			stoneId = 5; mySlot = 5;
+		}
+		else if (this.name == "greenstone3") {
+			stoneId = 4; mySlot = 4;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown() {
+		if (Input.GetKey("mouse 0")) {			
+			//print("myslot" + mySlot.ToString()); 
+			print("Game win = " + gameScript.Instance.minigame4SuccesfullyFinished());
+			print("Game lost = " + gameScript.Instance.minigame4Lost());
+
+			if(stoneId<=3){// yellow stone
+				if(mySlot<=5 &&  gameScript.Instance.minigame4slotFree(mySlot+1)){// move one down
+					print("Move one down!");
+					var position = this.gameObject.transform.position;
+					position.Set(position.x,position.y-1.42f,position.z);
+					this.gameObject.transform.position = position;
+					gameScript.Instance.setMinigame4slotFree(mySlot,0);
+					gameScript.Instance.setMinigame4slotFree(mySlot+1,stoneId);mySlot=mySlot+1;
+					//print(this.gameObject.transform.position.y);
+				}else if(mySlot<=4 &&   gameScript.Instance.minigame4slotFree(mySlot+2)){// move two down
+					print("Move two down!");
+					var position = this.gameObject.transform.position;
+					position.Set(position.x,position.y-2f*1.42f,position.z);
+					this.gameObject.transform.position = position;
+					gameScript.Instance.setMinigame4slotFree(mySlot,0);
+					gameScript.Instance.setMinigame4slotFree(mySlot+2,stoneId);mySlot=mySlot+2;
+				}else{ // you cant move
+					print("Cant move!");
+				}
+			}else{// green stone
+				if(mySlot>=1 && gameScript.Instance.minigame4slotFree(mySlot-1)){// move one up
+					print("Move one up!");
+					var position = this.gameObject.transform.position;
+					position.Set(position.x,position.y+1.42f,position.z);
+					this.gameObject.transform.position = position;
+					gameScript.Instance.setMinigame4slotFree(mySlot,0);
+					gameScript.Instance.setMinigame4slotFree(mySlot-1,stoneId);mySlot=mySlot-1;
+				}else if(mySlot>=2 && gameScript.Instance.minigame4slotFree(mySlot-2)){// move two up
+					print("Move two up!");
+					var position = this.gameObject.transform.position;
+					position.Set(position.x,position.y+2f*1.42f,position.z);
+					this.gameObject.transform.position = position;
+					gameScript.Instance.setMinigame4slotFree(mySlot,0);
+					gameScript.Instance.setMinigame4slotFree(mySlot-2,stoneId);mySlot=mySlot-2;
+				}else{ // you cant move
+					print("Cant move!");
+				}
+			} 
+		}
+	}
+}
---
+++ gamedev_unity/Assets/Scripts/gameScript.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+
+	public static gameScript Instance = null;
+
+	private int[] minigame4slots;
+	// Use this for initialization
+	void Start () {
+		minigame4slots = new int[7];
+		this.setMinigame4slotFree(0,1);
+		this.setMinigame4slotFree(1,2);
+		this.setMinigame4slotFree(2,3);
+		this.setMinigame4slotFree(3,0);
+		this.setMinigame4slotFree(4,4);
+		this.setMinigame4slotFree(5,5);
+		this.setMinigame4slotFree(6,6);
+	}
+
+	void Awake(){
+		Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool minigame4slotFree(int index){
+		if (minigame4slots [index] == 0)
+				return true;
+		else
+				return false;
+	}
+
+	public void setMinigame4slotFree(int index, int stone){
+		minigame4slots [index] = stone;
+	}
+
+	public bool minigame4SuccesfullyFinished(){
+		if (minigame4slots [0] == 4 &&
+						minigame4slots [1] == 5 &&
+						minigame4slots [2] == 6 &&
+						minigame4slots [4] == 1 &&
+						minigame4slots [5] == 2 &&
+						minigame4slots [6] == 3)
+						return true;
+				else
+						return false;
+	}
+	public bool minigame4Lost(){
+		if (this.minigame4SuccesfullyFinished()) {
+			return false;
+		}
+		for (int wantedStone=1; wantedStone<=6; wantedStone++) {
+			for(int i=0;i<7;i++){
+				if(minigame4slots[i]==wantedStone){
+					if(wantedStone<=3){// yellow
+						if(i<=5 && minigame4slots[i+1]==0){
+							return false;
+						}else if(i<=4 && minigame4slots[i+2]==0){
+							return false;
+						}
+					}
+					if(wantedStone>3){// green
+						if(i>=15 && minigame4slots[i-1]==0){
+							return false;
+						}else if(i>=2 && minigame4slots[i-2]==0){
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+}
